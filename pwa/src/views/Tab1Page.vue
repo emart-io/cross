d44274cfc3a7c1fa874a0c871607067daeeeeda5
Daniel <jmzwcn@gmail.com>
2023-02-17 10:08:51 +0800

    Update
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- pwa/src/views/Tab1Page.vue
+++ pwa/src/views/Tab1Page.vue
@@ -16,11 +16,11 @@
         @slideChange="onSlideChange" :pagination="{ clickable: true }">
         <swiper-slide>
           <!-- <ion-header collapse="condense">
-                                              <ion-toolbar>
-                                                <ion-title size="large">Tab 1</ion-title>
-                                              </ion-toolbar>
-                                            </ion-header>
-                                            <ExploreContainer name="Tab 1 page" /> -->
+                                                        <ion-toolbar>
+                                                          <ion-title size="large">Tab 1</ion-title>
+                                                        </ion-toolbar>
+                                                      </ion-header>
+                                                      <ExploreContainer name="Tab 1 page" /> -->
           <img alt="Silhouette of mountains" src="assets/0211.jpeg" />
         </swiper-slide>
         <swiper-slide>
@@ -28,44 +28,43 @@
           <img alt="Silhouette of mountains" src="assets/bujk.jpeg" />
         </swiper-slide>
         <swiper-slide>
-          <img alt="Silhouette of mountains" src="assets/pz-ser-banner.jpg" />
+          <img alt="Silhouette of mountains" src="assets/peizhen5.png" />
         </swiper-slide>
       </swiper>
-      <!-- <div id="container"></div> -->
+      <div id="container" style="display:none"></div>
       <ion-grid>
         <ion-row>
           <ion-col>
             <ion-card>
-              <!-- <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" /> -->
+              <img alt="Silhouette of mountains" src="assets/pz-ser-banner.jpg" />
               <ion-card-header>
                 <ion-card-title>就医陪诊</ion-card-title>
-                <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+                <ion-card-subtitle>暖心陪诊，安心</ion-card-subtitle>
               </ion-card-header>
-
-              <ion-card-content>
-                Here's a small text description
-              </ion-card-content>
             </ion-card>
           </ion-col>
           <ion-col>
             <ion-card>
-              <!-- <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" /> -->
+              <img alt="Silhouette of mountains" src="assets/pz-ser-banner.jpg" />
               <ion-card-header>
                 <ion-card-title>取送结果</ion-card-title>
-                <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+                <ion-card-subtitle>安全方便，快捷</ion-card-subtitle>
               </ion-card-header>
-
-              <ion-card-content>
-                Here's a small text description
-              </ion-card-content>
             </ion-card>
           </ion-col>
         </ion-row>
       </ion-grid>
       <ion-list>
-        <li v-for="item in hospitals" :key="JSON.stringify(item['id'])">
-          {{ item['name'] }}
-        </li>
+        <ion-item v-for="item in hospitals" :key="item['id']" lines="none">
+          <ion-thumbnail slot="start">
+            <img alt="Silhouette of mountains" :src="item['photos'][0]['url']" style="border-radius: 10px" />
+          </ion-thumbnail>
+          <ion-label>
+            <h3>{{ item['name'] }}</h3>
+            <p>地址:{{ item['address'] }}</p>
+            <p>电话:{{ item['tel'] }}</p>
+          </ion-label>
+        </ion-item>
       </ion-list>
     </ion-content>
 </ion-page>
@@ -73,11 +72,10 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonButton, IonIcon, IonList,/*IonTitle,*/ IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonGrid, IonRow } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonButton, IonIcon, IonList, IonLabel, IonContent, IonCard, /*IonCardContent,*/ IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonGrid, IonRow, IonItem, IonThumbnail } from '@ionic/vue';
 //import ExploreContainer from '@/components/ExploreContainer.vue';
 import { location } from 'ionicons/icons';
 import AMapLoader from '@amap/amap-jsapi-loader';
-// import { shallowRef } from '@vue/reactivity'
 import { Autoplay, Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/vue';
 
@@ -91,10 +89,10 @@ export default defineComponent({
     console.log('Home page will enter');
     this.getLocation();
   },
-  components: { /*ExploreContainer,*/ IonHeader, IonToolbar, IonButton, IonIcon, IonList, /*IonTitle,*/ IonContent, IonPage, Swiper, SwiperSlide, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonGrid, IonRow },
+  components: { /*ExploreContainer,*/ IonHeader, IonToolbar, IonButton, IonIcon, IonList, IonLabel, IonContent, IonPage, Swiper, SwiperSlide, IonCard, /*IonCardContent,*/ IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonGrid, IonRow, IonItem, IonThumbnail },
   data() {
     return {
-      position: "湖北荆门",
+      position: "北京市",
       autoplay: { delay: 3000 },
       hospitals: [],
     }
@@ -103,7 +101,7 @@ export default defineComponent({
     getLocation() {
       AMapLoader.load({
         key: '60d396703bef1a6a93d2eca45a70e764',
-        version: '1.4.15',
+        version: '2.0',
         plugins: ['AMap.Geolocation', 'AMap.PlaceSearch'],
       }).then(AMap => {
         const geolocation = new AMap.Geolocation({
@@ -116,31 +114,24 @@ export default defineComponent({
             console.log("111", result)
             if (result.addressComponent) {
               this.position = result.addressComponent.province;
-
-              var placeSearch = new AMap.PlaceSearch({
-                pageSize: 50, // 单页显示结果条数
-                // city 指定搜索所在城市，支持传入格式有：城市名、citycode和adcode
-                city: result.addressComponent.citycode,
-              });
-
-              placeSearch.search('三甲医院', (status: any, result: any) => {
-                // 查询成功时，result即对应匹配的POI信息
-                // console.log(JSON.stringify(result));
-                this.$data.hospitals = result.poiList.pois;
-              });
             }
           } else {
             console.log("222", result);
           }
         })
 
-        // this.map = new AMap.Map("container", {  //设置地图容器id
-        //   viewMode: "3D",    //是否为3D地图模式
-        //   zoom: 3,           //初始化地图级别
-        //   center: [105.602725, 37.076636], //初始化地图中心点位置
-        //   mapStyle: 'amap://styles/light', //设置地图的显示样式
-        // });
+        var placeSearch = new AMap.PlaceSearch({
+          pageSize: 100, // 单页显示结果条数
+          // city 指定搜索所在城市，支持传入格式有：城市名、citycode和adcode
+          city: '010',
+          panel: "container",
+        });
 
+        placeSearch.search('三甲医院', (status: any, result: any) => {
+          // 查询成功时，result即对应匹配的POI信息
+          console.log(JSON.stringify(result));
+          this.$data.hospitals = result.poiList.pois;
+        });
       }).catch((e) => {
         console.log(e)
       })
@@ -182,11 +173,4 @@ export default defineComponent({
   object-fit: cover;
   /* border-radius: 15px; */
 }
-
-/* #container {
-  padding: 0px;
-  margin: 0px;
-  width: 100%;
-  height: 60%;
-} */
 </style>
