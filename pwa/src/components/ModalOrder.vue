b818fed916e26876429471e0d769d4d38807f05a
Daniel <jmzwcn@gmail.com>
2023-05-10 22:34:46 +0800

    Update.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- pwa/src/components/ModalOrder.vue
+++ pwa/src/components/ModalOrder.vue
@@ -10,18 +10,32 @@
             </ion-buttons>
         </ion-toolbar>
     </ion-header>
-    <ion-content class="ion-padding">
+    <!-- <ion-content class="ion-padding"> -->
+    <ion-content>
         <ion-item>
-            <ion-label>名称：</ion-label>
-            <ion-input v-model="order.name" placeholder="请输入订单名" required></ion-input>
+            <ion-label>就诊医院</ion-label>
+            <!-- <ion-input v-model="order.location" placeholder="请输入医院名称" required></ion-input> -->
+            <ion-select mode="ios" aria-label="就诊医院" interface="action-sheet" placeholder="请选择就诊医院">
+                <ion-select-option value="apples">Apples</ion-select-option>
+                <ion-select-option value="oranges">Oranges</ion-select-option>
+                <ion-select-option value="bananas">Bananas</ion-select-option>
+            </ion-select>
         </ion-item>
         <ion-item>
-            <ion-label>地点：</ion-label>
-            <ion-input v-model="order.location" placeholder="请输入医院名称" required></ion-input>
+            <ion-label>就诊科室</ion-label>
+            <ion-input slot="end" v-model="order.name" placeholder="请输入就诊科室" required></ion-input>
         </ion-item>
         <ion-item>
-            <ion-label>附加信息：</ion-label>
-            <ion-textarea v-model="order.description" placeholder="可选" :rows="lines"></ion-textarea>
+            <ion-label>就诊时间</ion-label>
+            <ion-input slot="end" v-model="order.name" placeholder="请选择就诊时间" required></ion-input>
+        </ion-item>
+        <ion-item>
+            <ion-label>就诊人</ion-label>
+            <ion-input slot="end" v-model="order.name" placeholder="请选择就诊人" required></ion-input>
+        </ion-item>
+        <ion-item>
+            <ion-label>备注</ion-label>
+            <ion-textarea slot="end" v-model="order.description" :rows="lines"></ion-textarea>
         </ion-item>
     </ion-content>
 </template>
@@ -38,6 +52,8 @@ import {
     IonLabel,
     IonInput,
     IonTextarea,
+    IonSelect,
+    IonSelectOption,
     modalController,
 } from '@ionic/vue';
 import { defineComponent } from 'vue';
@@ -46,7 +62,7 @@ import { Order } from '../sdk/order_pb';
 
 export default defineComponent({
     name: 'ModalOrder',
-    components: { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonItem, IonLabel, IonInput, IonTextarea },
+    components: { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonItem, IonLabel, IonInput, IonTextarea, IonSelect, IonSelectOption },
     data() {
         var lines = 2;
         var order = new Order().toObject();
